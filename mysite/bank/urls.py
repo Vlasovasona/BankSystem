23ce8712a74664acebe9a1beab790feeddfc3bd8
Sofa Vlasova <sofa.vlasova2004@gmail.com>
2025-02-17 18:53:08 +0500

    5

--- mysite/bank/urls.py
+++ mysite/bank/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'bank'
 
 urlpatterns = [
-    # path('', views.clients_list, name='clients_list'),
     path('', views.ClientListView.as_view(), name='clients_list'),
     path('<int:client_code>/', views.client_detail, name='client_detail'),
     path('clients/', views.ClientListView.as_view(), name='clients_list'),

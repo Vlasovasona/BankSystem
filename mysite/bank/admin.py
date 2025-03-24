c8d8574ac031213723d040f51d37fce281ac571e
Sofa Vlasova <sofa.vlasova2004@gmail.com>
2025-03-24 21:31:58 +0500

    12

--- mysite/bank/admin.py
+++ mysite/bank/admin.py
@@ -2,8 +2,8 @@
 from django.contrib import admin
 from .models import AuthGroup, AuthGroupPermissions, AuthPermission, AuthUser
 from .models import AuthUserGroups, AuthUserUserPermissions
-# from .models import CreditType, Credits, DepositTypes, Deposits, DescriptiveStatistics
-# from .models import DjangoAdminLog, DjangoContentType, DjangoMigrations, DjangoSession, StatementOfDeposits, Statistics
+from .models import Clients, CreditStatement, LoanTypes, Payroll
+from .models import DjangoAdminLog, DjangoContentType, DjangoMigrations, DjangoSession
 
 
 
@@ -13,72 +13,46 @@ admin.site.register(AuthPermission)
 admin.site.register(AuthUser)
 admin.site.register(AuthUserGroups)
 admin.site.register(AuthUserUserPermissions)
-# admin.site.register(Clients)
-# admin.site.register(CreditStatement)
-# admin.site.register(CreditType)
-# admin.site.register(Credits)
-# admin.site.register(DepositTypes)
-# admin.site.register(Deposits)
-# admin.site.register(DescriptiveStatistics)
-# admin.site.register(DjangoAdminLog)
-# admin.site.register(DjangoContentType)
-# admin.site.register(DjangoMigrations)
-# admin.site.register(DjangoSession)
-# # admin.site.register(StatementOfDeposits)
-# admin.site.register(Statistics)
-
-# @admin.register(Clients)
-# class ClientsAdmin(admin.ModelAdmin):
-#     search_fields = ('familia', 'name', 'otchestvo')
-#     list_display = ('familia', 'name', 'otchestvo', 'adress', 'phone_number',
-#                     'age', 'gender', 'presence_absence_of_a_car',
-#                     'presence_absence_of_real_estate', 'month_income')
-#
-#     list_filter = ('gender', 'presence_absence_of_a_car',
-#                    'presence_absence_of_real_estate', 'month_income')
-#
-#     ordering = ('familia', 'name', 'otchestvo')
-#
-#
-# @admin.register(CreditStatement)
-# class CreditStatementAdmin(admin.ModelAdmin):
-#     search_fields = ('credit_code', 'client_code', 'loan_repayment_date', 'loan_issuance_date')
-#     list_display = ('loan_repayment_number', 'client_code',
-#                     'credit_code', 'credit_type_code', 'loan_issuance_date',
-#                     'loan_repayment_date', 'redemption_status')
-#
-#     list_filter = ('redemption_status', 'credit_type_code')
+admin.site.register(Clients)
+admin.site.register(CreditStatement)
+admin.site.register(LoanTypes)
+admin.site.register(Payroll)
+admin.site.register(DjangoAdminLog)
+admin.site.register(DjangoContentType)
+admin.site.register(DjangoMigrations)
+admin.site.register(DjangoSession)
 
 
-# @admin.register(CreditType)
-# class CreditTypeAdmin(admin.ModelAdmin):
-#     list_display = ('credit_type_code', 'credit_type_name', 'credit_percent')
-#
-#
-# @admin.register(Credits)
-# class CreditsAdmin(admin.ModelAdmin):
-#     list_display = ('credit_code', 'credit_amount', 'term_month',
-#                     'monthly_payment_amount')
-#     list_filter = ('credit_amount', 'monthly_payment_amount')
-#
-#
-# @admin.register(DepositTypes)
-# class DepositTypesAdmin(admin.ModelAdmin):
-#     list_display = ('deposit_type_code', 'name_of_deposit_type',
-#                     'deposit_percent')
-#
-#
-#
-# @admin.register(Deposits)
-# class DepositsAdmin(admin.ModelAdmin):
-#     list_display = ('deposit_code', 'deposit_amount')
-#
-#
-# @admin.register(StatementOfDeposits)
-# class StatementOfDepositsAdmin(admin.ModelAdmin):
-#     search_fields = ('deposit_closing_number', 'deposit_opening_date',
-#                      'deposit_closing_status', 'client_code', 'deposit_code',
-#                      'deposit_type_code')
-#     list_display = ('deposit_closing_number', 'deposit_opening_date',
-#                     'deposit_ending_date', 'deposit_closing_status',
-#                     'client_code', 'deposit_code', 'deposit_type_code')
+
+@admin.register(Clients)
+class ClientsAdmin(admin.ModelAdmin):
+    search_fields = ('surname', 'name', 'patronymic', 'passport_serial_number')
+    list_display = ('surname', 'name', 'patronymic', 'adress', 'phone_number',
+                    'age', 'sex', 'flag_own_car',
+                    'flag_own_property', 'month_income', 'count_children',
+                    'education_type', 'passport_serial_number'
+                    )
+
+    list_filter = ('gender', 'flag_own_car',
+                   'flag_own_property', 'month_income')
+
+    ordering = ('surname', 'name', 'patronymic')
+
+
+@admin.register(CreditStatement)
+class CreditStatementAdmin(admin.ModelAdmin):
+    search_fields = ('number_of_the_loan_agreement', 'loan_opening_date', 'loan_type')
+    list_display = ('number_of_the_loan_agreement', 'credit_amount',
+                    'term_month', 'monthly_payment', 'loan_opening_date',
+                    'repayment_status', 'loan_type', 'client')
+
+    list_filter = ('redemption_status', 'credit_type_code')
+
+
+@admin.register(LoanTypes)
+class CreditTypeAdmin(admin.ModelAdmin):
+    list_display = ('registration_number', 'name_of_the_type', 'interest_rate')
+
+@admin.register(Payroll)
+class PayrollAdmin(admin.ModelAdmin):
+    list_display = ('loan', 'payment_date', 'payment_status')
